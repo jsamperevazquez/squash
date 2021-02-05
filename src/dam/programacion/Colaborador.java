01d1ad25b43d7756e55007eaeb9492a644fc407f
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-02-05 09:46:24 +0100

    Hago un cambio a la clase Colaborador para añadir otro metodo

--- src/dam/programacion/Colaborador.java
+++ src/dam/programacion/Colaborador.java
@@ -4,4 +4,7 @@ public class Colaborador {
     public void primerMetodo(){
         System.out.println("Primer metodo de la clase colaborador");
     }
+    public void segundoMetodo(){
+        System.out.println("Segundo metodo de la clase colaborador");
+    }
 }
