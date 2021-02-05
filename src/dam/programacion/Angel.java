641ca620091b9b1fc99b40918ec3a9b28629595f
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-02-05 09:45:19 +0100

    Creado método para ver lider

---
+++ src/dam/programacion/Angel.java
@@ -0,0 +1,12 @@
+package dam.programacion;
+
+/**
+ * Creado por @autor: angel
+ * El  05 de feb. de 2021.
+ **/
+public class Angel {
+    public void verLider(){
+        System.out.println("Jose Ángel Sampere Vázquez");
+    }
+
+}
